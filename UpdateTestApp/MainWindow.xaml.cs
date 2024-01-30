16550d5c84f90ae6dc25c101c627e8852cc176e7
Fangmaci <61710640+Fangmaci@users.noreply.github.com>
2024-01-30 17:19:56 +0100

    Updated code

--- UpdateTestApp/MainWindow.xaml.cs
+++ UpdateTestApp/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Squirrel;
 
 namespace UpdateTestApp
 {
@@ -16,11 +17,41 @@ namespace UpdateTestApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        UpdateManager manager;
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
         }
 
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            manager = await UpdateManager
+                .GitHubUpdateManager(@"https://github.com/Fangmaci/UpdateTestApp");
+            CurrentlyInstalledVersion.Text = manager.CurrentlyInstalledVersion().ToString();
+        }
 
+        private async void CheckForUpdatesButton_Click(object sender, RoutedEventArgs e)
+        {
+            var updateInfo = await manager.CheckForUpdate();
+
+            if(updateInfo.ReleasesToApply.Count >0) 
+            {
+                CheckForUpdatesButton.IsEnabled = true;
+            }
+            else
+            {
+                CheckForUpdatesButton.IsEnabled= false;
+            }
+        }
+
+        private async void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            await manager.UpdateApp();
+
+            MessageBox.Show("Updated successfully");
+        }
     }
 }
